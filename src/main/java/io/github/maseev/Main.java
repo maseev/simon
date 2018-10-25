2245110ae33a8b51c9107d68a12b0fe7e96f13e9
Miron Aseev <aseev.miron@gmail.com>
2018-10-25 16:24:11 +0700

    Add logging to the main app class

--- src/main/java/io/github/maseev/Main.java
+++ src/main/java/io/github/maseev/Main.java
@@ -20,13 +20,23 @@ public class Main {
     String website = "https://simonstalenhag.se/";
     String imagesFolder = "images";
 
+    log.info("extracting images from the page");
     Document doc = Jsoup.connect(website).get();
     Elements anchorElements = new ImageExtractor(doc).extract();
+    log.info("done");
+
+    log.info("partitioning images");
     List<ImagePartition> imagePartitions = new ImagePartitioner(anchorElements).partition();
+    log.info("done");
+
+    log.info("downloading and partitioning images");
     List<ImageFilePartition> imageFilePartitions =
       new ImageFilePartitioner(imagesFolder, imagePartitions, new ImageDownloader()).partition();
+    log.info("done");
 
+    log.info("generating the HTML page");
     PageGenerator pageGenerator = new PageGenerator(imageFilePartitions);
     pageGenerator.generate("page.mustache");
+    log.info("done");
   }
 }
