0c44a83dd535fbb4e9b29e9a1e81de07a1b9900b
Miron Aseev <aseev.miron@gmail.com>
2018-10-29 17:54:11 +0700

    Speed up the image downloading process by using the thread pool

--- src/main/java/io/github/maseev/partitioner/file/ImageFilePartitioner.java
+++ src/main/java/io/github/maseev/partitioner/file/ImageFilePartitioner.java
@@ -11,49 +11,78 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ImageFilePartitioner {
+
+  static final class DaemonThreadFactory implements ThreadFactory {
+
+    @Override
+    public Thread newThread(Runnable r) {
+      Thread t = new Thread(r);
+      t.setDaemon(true);
+      t.setName("ImagePartitioningTask");
+      return t;
+    }
+  }
 
   private static final Logger log = LogManager.getLogger();
 
   private final String imagesFolder;
   private final List<ImagePartition> imagePartitions;
   private final ImageDownloader imageDownloader;
+  private final ExecutorService threadPool;
 
   public ImageFilePartitioner(String imagesFolder, List<ImagePartition> imagePartitions,
                               ImageDownloader imageDownloader) {
     this.imagesFolder = imagesFolder;
     this.imagePartitions = imagePartitions;
     this.imageDownloader = imageDownloader;
+
+    threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors(),
+      new DaemonThreadFactory());
   }
 
-  public List<ImageFilePartition> partition() throws IOException {
+  public List<ImageFilePartition> partition() throws IOException, ExecutionException, InterruptedException {
     List<ImageFilePartition> partitions = new ArrayList<>(imagePartitions.size());
+    List<Future<ImageFilePartition>> futures = new ArrayList<>(imagePartitions.size());
 
     for (ImagePartition imagePartition : imagePartitions) {
-      String dstFolder = generateFolderName(imagesFolder, imagePartition.getBigImage());
-      String bigImageFilename = extractFilename(imagePartition.getBigImage());
-      String thumbnailFilename = extractFilename(imagePartition.getThumbnail());
-      String bigImageDstPath = generateFilePath(dstFolder, bigImageFilename);
-      String thumbnailDstPath;
+      Future<ImageFilePartition> future = threadPool.submit(() -> {
+        String dstFolder = generateFolderName(imagesFolder, imagePartition.getBigImage());
+        String bigImageFilename = extractFilename(imagePartition.getBigImage());
+        String thumbnailFilename = extractFilename(imagePartition.getThumbnail());
+        String bigImageDstPath = generateFilePath(dstFolder, bigImageFilename);
+        String thumbnailDstPath;
 
-      if (bigImageFilename.equals(thumbnailFilename)) {
-        thumbnailDstPath = generateFilePath(dstFolder, append(thumbnailFilename, "thumbnail"));
-      } else {
-        thumbnailDstPath = generateFilePath(dstFolder, thumbnailFilename);
-      }
+        if (bigImageFilename.equals(thumbnailFilename)) {
+          thumbnailDstPath = generateFilePath(dstFolder, append(thumbnailFilename, "thumbnail"));
+        } else {
+          thumbnailDstPath = generateFilePath(dstFolder, thumbnailFilename);
+        }
 
-      new File(dstFolder).mkdirs();
+        new File(dstFolder).mkdirs();
 
-      File bigImage = imageDownloader.download(imagePartition.getBigImage(), bigImageDstPath);
-      File thumbnail = imageDownloader.download(imagePartition.getThumbnail(), thumbnailDstPath);
+        File bigImage = imageDownloader.download(imagePartition.getBigImage(), bigImageDstPath);
+        File thumbnail = imageDownloader.download(imagePartition.getThumbnail(), thumbnailDstPath);
 
-      partitions.add(new ImageFilePartition(bigImage, thumbnail));
+        log.info("image and its thumbnail have been downloaded; image url: {}",
+          imagePartition.getBigImage());
 
-      log.info("image and its thumbnail have been downloaded; image url: {}",
-        imagePartition.getBigImage());
+        return new ImageFilePartition(bigImage, thumbnail);
+      });
+
+      futures.add(future);
+    }
+
+    for (Future<ImageFilePartition> future : futures) {
+      partitions.add(future.get());
     }
 
     return partitions;
